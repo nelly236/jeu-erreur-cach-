feea7c728ec942226fc15116e5ba7075b8f00994
nelly236 <nynelly5@gmail.com>
2022-02-15 16:37:43 +0100

    feat: structure + logique js

---
+++ app.js
@@ -0,0 +1,35 @@
+let hublot = document.getElementById('hublot');
+
+hublot.addEventListener("click", function(){
+    hublot.style.opacity="1";
+});
+
+let sac = document.getElementById('sac');
+
+sac.addEventListener("click", function(){
+    sac.style.opacity="1";
+});
+
+let sigle = document.getElementById('sigle');
+
+sigle.addEventListener("click", function(){
+    sigle.style.opacity="1";
+});
+
+let symbole = document.getElementById('symbole');
+
+symbole.addEventListener("click", function(){
+    symbole.style.opacity="1";
+});
+
+let roue = document.getElementById('roue');
+
+roue.addEventListener("click", function(){
+    roue.style.opacity="1";
+});
+
+let racine = document.getElementById('racine');
+
+racine.addEventListener("click", function(){
+    racine.style.opacity="1";
+});
